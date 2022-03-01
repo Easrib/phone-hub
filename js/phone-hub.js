f5cd5251564db4cf538246ac4848455a08bde829
Easrib <easrib.sharik01@gmail.com>
2022-03-01 07:19:04 +0600

    search result area

--- js/phone-hub.js
+++ js/phone-hub.js
@@ -1,8 +1,34 @@
-const searchInput = () => {
-    const searchField = document.getElementById('search-field').value;
-    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchField}`
+// search bar js 
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(json => displaySearchResult(json.data))
+
+    searchField.value = '';
+}
+
+// search results area 
+const displaySearchResult = (data) => {
+
+    const searchResult = document.getElementById('search-result');
+
+    data.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+                    <div class="card h-100">
+                        <img src="${phone.image}" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Name: ${phone.phone_name}</h5>
+                            <p class="card-text">Brand: ${phone.brand}</p>
+                            <button type="button" class="btn btn-success">Details</button>
+                        </div>
+                    </div>`
+        searchResult.appendChild(div);
+    });
+
 }
